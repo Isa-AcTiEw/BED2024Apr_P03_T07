b1c822351d9a4b519d53de7aacee08cf13a5210c
Isa-AcTiEw <s10257760@connect.np.edu.sg>
2024-06-27 13:56:31 +0800

    Worked on validation for event

--- middleware/validateEvent.js
+++ middleware/validateEvent.js
@@ -1,8 +1,17 @@
 const Joi = require('joi');
 const validateBook = (req, res, next) => {
     const schema = Joi.object({
-      title: Joi.string().min(3).max(50).required(),
-      author: Joi.string().min(3).max(50).required(),
+    //   title: Joi.string().min(3).max(50).required(),
+    //   author: Joi.string().min(3).max(50).required(),
+    EventID: Joi.string().min(10).max(10).required(),
+    EventName: Joi.string().min(30).max(100).required(),
+    EventDesc: Joi.string().min(30).max(300).required(),
+    EventPrice: Joi.number.required(),
+    EventCat: Joi.string().min(30).max(100).required(),
+    EventLocation: Joi.string().min(10).max(30).required(),
+    EventRegEndDate: Joi.date.required(),
+    EventMgrID: Joi.string().min(10).max(10).required(),
+    EventIntake: Joi.number.required(),
     });
   
     const validation = schema.validate(req.body, { abortEarly: false }); // Validate request body
