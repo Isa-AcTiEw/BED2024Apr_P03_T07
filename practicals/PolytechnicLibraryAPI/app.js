f989e472ac480f5e1fab2f48093e489ca7135890
Ming Qi <s10257808@connect.np.edu.sg>
2024-07-03 20:15:29 +0800

    register user done

--- practicals/PolytechnicLibraryAPI/app.js
+++ practicals/PolytechnicLibraryAPI/app.js
@@ -1,11 +1,48 @@
-const express = require('express');
-const mssql = require('mssql');
+const express = require("express");
+const sql = require("mssql");
+const dbConfig = require("./config/DbConfigPractical");
+const bodyParser = require("body-parser");
+const verifyJWT = require('./middleware/authorizeTokenAndRoles');
+const userController = require('./controller/userController');
+const booksController = require('./controller/booksController');
+require('dotenv').config();
+const scretKey = process.env.SECRET_KEY; 
+
 
 const app = express();
-const port = process.env.PORT || 2000; // Use process.env.PORT for dynamic port or default to 2000
+const port = 2000;
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+// Include body-parser middleware to handle JSON data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
+
+// Routes
+app.post('/register', userController.registerUser);
+app.post('/login', userController.login);
+
+app.get('/books', verifyJWT, booksController.getAllBooks);
+app.put('/books/:bookId/availability', verifyJWT, booksController.updateAvailability);
+
+
+app.listen(port, async () => {
+  try {
+    // Connect to the database
+    await sql.connect(dbConfig);
+    console.log("Database connection established successfully");
+  } catch (err) {
+    console.error("Database connection error:", err);
+    // Terminate the application with an error code (optional)
+    process.exit(1); // Exit with code 1 indicating an error
+  }
+
+  console.log(`Server listening on port ${port}`);
 });
 
-
+// Close the connection pool on SIGINT signal
+process.on("SIGINT", async () => {
+  console.log("Server is gracefully shutting down");
+  // Perform cleanup tasks (e.g., close database connections)
+  await sql.close();
+  console.log("Database connection closed");
+  process.exit(0); // Exit with code 0 indicating successful shutdown
+});
--- practicals/PolytechnicLibraryAPI/config/DbConfigPractical.js
+++ practicals/PolytechnicLibraryAPI/config/DbConfigPractical.js
@@ -1,1 +1,12 @@
-"hi" 
+require('dotenv').config();   
+module.exports = {
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    server: "localhost",
+    database: process.env.DB_NAME,
+    trustServerCertificate: true,
+    options: {
+        port: 1433, // Default SQL server port
+        connectionTimeout: 60000, // Connection Timeout
+    },
+};
