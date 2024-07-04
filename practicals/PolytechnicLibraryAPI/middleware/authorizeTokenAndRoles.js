cbe7fc07a05e8d6b05c37330c9e966114b579682
Ming Qi <s10257808@connect.np.edu.sg>
2024-07-04 16:39:31 +0800

    Commiting new changes added git ignore and practicals

---
+++ practicals/PolytechnicLibraryAPI/middleware/authorizeTokenAndRoles.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+require('dotenv').config({ path: '../.env' });
+const secretKey = process.env.ACCESS_TOKEN_SECRET;
+function verifyJWT(req, res, next) {
+  const token = req.headers.authorization && req.headers.authorization.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({token:token });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+
+    const authorizedRoles = {
+      "/books": ["member", "librarian"], // Anyone can view books
+      "/books/[0-9]+/availability": ["librarian"], // Only librarians can update availability
+    };
+
+    const requestedEndpoint = req.url;
+    const userRole = decoded.role;
+
+    const authorizedRole = Object.entries(authorizedRoles).find(
+      ([endpoint, roles]) => {
+        const regex = new RegExp(`^${endpoint}$`); // Create RegExp from endpoint
+        return regex.test(requestedEndpoint) && roles.includes(userRole);
+      }
+    );
+
+    if (!authorizedRole) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+
+    req.user = decoded; // attach decoded user information to the request object
+    next();
+  });
+}
+
+module.exports = verifyJWT;
