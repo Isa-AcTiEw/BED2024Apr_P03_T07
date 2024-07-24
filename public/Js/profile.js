e07ea04d49dcdecd655f5315af7f9ab121d93c68
Ming Qi <s10257808@connect.np.edu.sg>
2024-07-24 18:06:09 +0800

    update profile

--- public/Js/profile.js
+++ public/Js/profile.js
@@ -21,7 +21,7 @@ profileForm.addEventListener('submit', async function(e) {
     updateProfile();
     setTimeout(() => {
         location.reload(); 
-    }, 1500); 
+    }, 2000); 
 });
 
 async function updateProfile() {
@@ -75,7 +75,7 @@ async function updateProfile() {
 function formatDate(dateString) {
     const date = new Date(dateString);
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+    const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
 }
@@ -90,5 +90,5 @@ function showAlert(type, msg) {
     alertPlaceholder.appendChild(alert);
     setTimeout(() => {
         alert.remove();
-    }, 5000);
+    }, 2000);
 }
